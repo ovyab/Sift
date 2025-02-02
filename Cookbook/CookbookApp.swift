36bb69bad1494e6f225d7bb214bb1e3fe20d4d8b
Ovya Barani <obarani@uci.edu>
2025-02-02 16:53:55 -0600

    Initial Commit

---
+++ Cookbook/CookbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookbookApp.swift
+//  Cookbook
+//
+//  Created by Ovya Barani on 2/2/25.
+//
+
+import SwiftUI
+
+@main
+struct CookbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
