d01194fc644860bd6698ff2dc0dec4e43084cff3
Ovya Barani <obarani@uci.edu>
2025-02-13 14:50:25 -0600

    Add API key to gitignore

---
+++ Sift/Config.example.swift
@@ -0,0 +1,5 @@
+// This is an example configuration file.
+// Copy this file to Config.swift and replace the API key with your own.
+struct ExampleConfig {
+    static let openAIKey = "YOUR_OPENAI_API_KEY_HERE"
+} 
